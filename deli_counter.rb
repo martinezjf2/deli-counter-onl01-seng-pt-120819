9261b8dd9962d4f565fdc6c9b9752107ff0f38be
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-18 01:33:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   puts "The line is currently empty." if katz_deli.length == 0
-  string = "The linen is currently:"
+  string = "The line is currently:"
   katz_deli.each_with_index do |name, index|
     string << "#{index+1}. #{name}"
   end
