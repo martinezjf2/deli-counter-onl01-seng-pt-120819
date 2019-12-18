39a6f1aa5c1eb4490cc0697c949adb48653b1492
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-18 01:35:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 def line(katz_deli)
   puts "The line is currently empty." if katz_deli.length == 0
+  return if katz_deli.length == 0
+
   string = "The line is currently:"
   katz_deli.each_with_index do |name, index|
     string << "#{index+1}. #{name}"
